f74f7a5ef8857086761961975b369f4e39b67f77
zzzj <1152092752@qq.com>
2020-08-01 09:26:23 +0800

    补充

--- chapter2/2.1-2.2/02-推导式代替filter和map.py
+++ chapter2/2.1-2.2/02-推导式代替filter和map.py
@@ -3,7 +3,10 @@ symbols = '$¢£¥€¤a'
 # 使用for循环 + map + filter
 
 iterator = filter(lambda symbol: symbol > 127, map(ord, symbols))
+# [162, 163, 165, 8364, 164]
 print(list(iterator))
 
-
 # 使用列表推导式
+
+# [162, 163, 165, 8364, 164]
+print([ord(symbol) for symbol in symbols if ord(symbol) > 127])
